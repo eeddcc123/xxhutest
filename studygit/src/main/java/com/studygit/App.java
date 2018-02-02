9a78420695241ef5ac6b55724444c7921d9e34c3
xxhu <huxinxinshiwo@163.com>
2018-02-02 10:39:04 +0800

    信息提交

---
+++ studygit/src/main/java/com/studygit/App.java
@@ -0,0 +1,16 @@
+package com.studygit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	System.out.println("我们是2害虫，我们是22害虫");
+        System.out.println( "Hello World!" );
+        
+    }
+}
---
+++ studygit/src/main/java/com/studygit/Tetst.java
@@ -0,0 +1,10 @@
+package com.studygit;
+
+public class Tetst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(111);
+	}
+
+}
